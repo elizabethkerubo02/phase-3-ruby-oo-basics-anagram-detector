80d2f1d9ad6b57212c01407f19de263ff8a81d5f
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-05-26 13:08:55 +0300

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+  
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(words)
+      words.select { |w| anagram?(w) } # Select words that are anagrams of the initialized word
+    end
+  
+    private
+  
+    def anagram?(other_word)
+      sort_word(word) == sort_word(other_word) && word.downcase != other_word.downcase
+      # Compare the sorted characters of both words
+      # Ensure they are equal and exclude identical words (case-insensitive)
+    end
+  
+    def sort_word(word)
+      word.downcase.chars.sort
+      # Sort the characters of a word in alphabetical order (case-insensitive)
+    end
+  end
+  
